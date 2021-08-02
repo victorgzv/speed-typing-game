eedca0fc8baea54a771eb8a6e1a5518ebb5af6d2
Victor <victorgonvilla@gmail.com>
2021-08-02 17:27:24 +0100

    added text state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 
-import React from "react"
+import React,{useState} from "react"
 
 function App() {
+  const [text, setText] = useState("")
+    
+  function handleChange(e) {
+      const {value} = e.target
+      setText(value)
+  }
+
+  console.log(text)
     return (
         <div>
             <h1>How fast do you type?</h1>
-            <textarea />
+            <textarea 
+            value ={text}
+            onChange ={handleChange}/>
             <h4>Time reminaing: ???</h4>
             <button>Start</button>
             <h1>Word count: ???</h1>
