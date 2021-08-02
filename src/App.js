6dfacfd330966a026a78eadb85ef33af3b00c219
Victor <victorgonvilla@gmail.com>
2021-08-02 18:26:30 +0100

    create state for isTimeRunning

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import React,{useState,useEffect} from "react"
 function App() {
   const [text, setText] = useState("")
   const [timeRemaining, setTimeRemaining] = useState(5)
-
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
     
   function handleChange(e) {
       const {value} = e.target
       setText(value)
   }
 
-  function calculateWordCount(text) {
-    const wordsArr = text.trim().split(" ")
-    return wordsArr.filter(word => word !== "").length
-}
+  // function calculateWordCount(text) {
+  //   const wordsArr = text.trim().split(" ")
+  //   return wordsArr.filter(word => word !== "").length
+  // }
 
 useEffect(() => {
-  if(timeRemaining > 0) {
+  if(isTimeRunning && timeRemaining > 0) {
       setTimeout(() => {
           setTimeRemaining(time => time - 1)
       }, 1000)
+  } else if(timeRemaining === 0){
+    setIsTimeRunning(false)
   }
-}, [timeRemaining])
+}, [timeRemaining,isTimeRunning])
 
 
   console.log(text)
@@ -34,7 +36,7 @@ useEffect(() => {
             value ={text}
             onChange ={handleChange}/>
             <h4>Time reminaing: {timeRemaining}</h4>
-            <button onClick={() => console.log(calculateWordCount(text))}>Start</button>
+            <button onClick={() => setIsTimeRunning(true)}>Start</button>
             <h1>Word count: ???</h1>
         </div>
     )
